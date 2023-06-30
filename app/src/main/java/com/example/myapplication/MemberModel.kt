b1837c32c9f5096e0d400829e0b55f50cdf48372
Park-HyeonJi <phjgina0314@gmail.com>
2023-06-30 13:52:24 +0900

    ..

---
+++ app/src/main/java/com/example/myapplication/MemberModel.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+class MemberModel (
+    var docId: String? = null,
+    var email: String? = null,
+    var nickname: String? = null,
+    var deviceId: String? = null
+    )
