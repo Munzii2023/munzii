b86764cd67dbbb01e6ad8cf673083a19ba9c0b38
hyunsoo <phs1289@gmail.com>
2023-07-07 18:40:43 +0900

    7/7 현 위치상에 marker 띄우는 것까지는 했어요..................................  => marker 움직이는 거 도와주실분

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,21 +7,16 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.TextView
-import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import androidx.databinding.ObservableMap.OnMapChangedCallback
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.databinding.NavigationHeaderBinding
 import com.google.android.material.navigation.NavigationView
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.util.FusedLocationSource
-import org.w3c.dom.Text
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
@@ -36,6 +31,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         ACCESS_COARSE_LOCATION
     )
     lateinit var mLocationSource: FusedLocationSource
+
+    //현재 위치 저장
     private var lat by Delegates.notNull<Double>()
     private var lon by Delegates.notNull<Double>()
 
@@ -129,6 +126,21 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
+    private fun setMark(marker: Marker, lat: Double, lng: Double, resourceID: Int) { //마커 띄우기
+        // 원근감 표시
+        marker.isIconPerspectiveEnabled = true
+        // 아이콘 지정
+        marker.icon = OverlayImage.fromResource(resourceID)
+        // 마커의 투명도
+        marker.alpha = 0.8f
+        // 마커 위치
+        marker.position = LatLng(lat, lng)
+        // 마커 우선순위
+        marker.zIndex = 10
+        // 마커 표시
+        marker.map = naverMap
+    }
+
     override fun onMapReady(naverMap: NaverMap) { //네이버 지도의 이벤트를 처리하는 콜백함수
         // NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
         // 지도상에 마커 표시
@@ -144,15 +156,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST_CODE);
 
 
-        /*val cameraPosition = CameraPosition(
+        /* val cameraPosition = CameraPosition(
             LatLng(37.65178832823347, 127.01614801495204), //위치 지정
             16.0 // 줌레벨 => 추가로 기울임 각도, 방향 설정 가능
         )
-        naverMap.cameraPosition = cameraPosition  //최초위치 설정*/
+        naverMap.cameraPosition = cameraPosition  //최초위치 설정 */
 
         naverMap.addOnLocationChangeListener { location ->
             lat = location.latitude
             lon = location.longitude
+            setMark(marker, lat, lon, R.drawable.baseline_place_24)
 
             /*Toast.makeText(
                 applicationContext,
@@ -198,6 +211,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onLowMemory()
         mapView.onLowMemory()
     }
+
 
 
 
