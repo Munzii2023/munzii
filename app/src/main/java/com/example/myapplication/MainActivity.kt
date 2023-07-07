658d4242397c02f5ce09e3e2d586c664238d5382
hyunsoo <phs1289@gmail.com>
2023-07-07 17:59:16 +0900

    7/7 네이버지도 띄우는거 + 디폴트로 현 위치 불러와서 지도에 띄우는 거 했습니데이.........

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,15 +11,33 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.databinding.ObservableMap.OnMapChangedCallback
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.databinding.NavigationHeaderBinding
 import com.google.android.material.navigation.NavigationView
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
 import org.w3c.dom.Text
+import kotlin.properties.Delegates
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
+    OnMapReadyCallback {
     lateinit var binding: ActivityMainBinding
     lateinit var _binding : NavigationHeaderBinding
-
+    private lateinit var mapView: MapView
+    private lateinit var naverMap: NaverMap
+    private var PERMISSION_REQUEST_CODE = 100;
+    private val PERMISSIONS = arrayOf(
+        ACCESS_FINE_LOCATION,
+        ACCESS_COARSE_LOCATION
+    )
+    lateinit var mLocationSource: FusedLocationSource
+    private var lat by Delegates.notNull<Double>()
+    private var lon by Delegates.notNull<Double>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +46,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         setContentView(binding.root)
         binding.mainDrawer.setNavigationItemSelectedListener(this)
+
+        // 네이버 지도
+        mapView = findViewById(R.id.navermap_view)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
+        // 위치를 반환하는 구현체인 FusedLocationSource 생성
+        mLocationSource = FusedLocationSource(this, PERMISSION_REQUEST_CODE)
 
     }
 
@@ -43,6 +72,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onResume() {
         super.onResume()
+        mapView.onResume()
         updateNavigationHeader()
     }
 
@@ -50,6 +80,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Intent에서 finish() 돌아올 때 실행
         // onCreate -> onStart -> onCreateOptionsMenu
         super.onStart()
+        mapView.onStart()
         if(_binding.headerEmail.text.equals("로그인이 필요합니다")){
             if(MyApplication.checkAuth()){
                 _binding.headerEmail!!.text= MyApplication.email
@@ -98,4 +129,76 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
+    override fun onMapReady(naverMap: NaverMap) { //네이버 지도의 이벤트를 처리하는 콜백함수
+        // NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
+        // 지도상에 마커 표시
+        val marker = Marker()
+        marker.position = LatLng(37.5670135, 126.9783740)
+        marker.map = naverMap
+
+        this.naverMap = naverMap
+        naverMap.locationSource = mLocationSource
+        //naverMap.setLocationSource(mLocationSource);
+
+        // 권한확인. 결과는 onRequestPermissionsResult 콜백 매서드 호출
+        ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST_CODE);
+
+
+        /*val cameraPosition = CameraPosition(
+            LatLng(37.65178832823347, 127.01614801495204), //위치 지정
+            16.0 // 줌레벨 => 추가로 기울임 각도, 방향 설정 가능
+        )
+        naverMap.cameraPosition = cameraPosition  //최초위치 설정*/
+
+        naverMap.addOnLocationChangeListener { location ->
+            lat = location.latitude
+            lon = location.longitude
+
+            /*Toast.makeText(
+                applicationContext,
+                "$lat, $lon",
+                Toast.LENGTH_SHORT
+            ).show()*/
+        }
+
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // request code와 권한획득 여부 확인
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                naverMap.setLocationTrackingMode(LocationTrackingMode.Follow)
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+
+
+}
