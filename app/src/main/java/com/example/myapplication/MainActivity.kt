64604dfd62c6e34e0188a5524de6e8d00818792b
박현수 <phs1289@gmail.com>
2023-11-08 15:13:10 +0900

    MainActivity.kt 코드 정리 및 주석 처리

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -45,6 +45,7 @@ import java.util.*
 import kotlin.properties.Delegates
 import android.view.View
 import com.example.myapplication.PhotoActivity
+import com.google.android.play.integrity.internal.t
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -66,8 +67,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var marker : Marker
     private val mMarkerList = arrayOfNulls<Marker>(700) //공공데이터에서 불러오는 미세먼지 마커들
-
-    //현재 TM 좌표
+    //현재 TM 좌표 전역에 보관
     private var tmX by Delegates.notNull<Double>()
     private var tmY by Delegates.notNull<Double>()
     //처음 위치 저장
@@ -76,7 +76,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private var latelyPosition : LatLng = LatLng(37.5670135, 126.9783740)
     private var lat by Delegates.notNull<Double>()
     private var lon by Delegates.notNull<Double>()
-
     // 코루틴 생성
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
@@ -100,18 +99,15 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         binding.navBtn.setOnClickListener {
             binding.drawer.openDrawer(GravityCompat.START)
         }
-
+        // floating btn 초기화
         mContext = applicationContext
         fabOpen = AnimationUtils.loadAnimation(mContext, R.anim.fab_open)
         fabClose = AnimationUtils.loadAnimation(mContext, R.anim.fab_close)
-
         fabMain = findViewById(R.id.ic_main)
         fabSub1 = findViewById(R.id.fab_sub1)
         fabSub2 = findViewById(R.id.fab_sub2)
-
         fabSub1.visibility = View.GONE
         fabSub2.visibility = View.GONE
-
         binding.icMain.setOnClickListener { v: View ->
             when (v.id) {
                 R.id.ic_main -> toggleFab()
@@ -123,29 +119,26 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 }
             }
         }
-
         binding.fabSub1.setOnClickListener{
             val intent = Intent(this, PhotoActivity::class.java)
             intent.putExtra("fromCamera", true)
             startActivity(intent)
         }
-
         binding.fabSub2.setOnClickListener{
             val intent = Intent(this, PhotoActivity::class.java)
             intent.putExtra("fromGallery", true)
             startActivity(intent)
         }
 
-
+        // 택배차량 지도로 연결
         binding.btnDeliveryVehicle.setOnClickListener {
             // SecondActivity로 이동하는 인텐트 시작
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
 
-        // 네이버 지도
+        // 네이버 지도 이용
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
         mapView = findViewById(R.id.navermap_view)
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
@@ -153,6 +146,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // 위치를 반환하는 구현체인 FusedLocationSource 생성
         mLocationSource = FusedLocationSource(this, PERMISSION_REQUEST_CODE)
 
+        // 검색 기능
         binding.mapsSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
 
@@ -186,8 +180,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
         })
     }
-
-
     private fun toggleFab() {
         if (isFabOpen) {
             fabMain.setImageResource(R.drawable.baseline_camera_alt_24)
@@ -210,23 +202,18 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-
-
     //위도경도 좌표계 => tm좌표 변환 함수
     private fun getTm(){
         val wgsPt = CoordPoint(naverMap.cameraPosition.target.longitude, naverMap.cameraPosition.target.latitude)
         Log.d("mobileApp", wgsPt.x.toString())
         val tmPt = TransCoord.getTransCoord(wgsPt, TransCoord.COORD_TYPE_WGS84,TransCoord.COORD_TYPE_TM)
         Log.d("mobileApp", tmPt.x.toString())
-        tmX = tmPt.x
-        tmY = tmPt.y
+        tmX = tmPt.x // 전역 변수에 tm좌표 저장
+        tmY = tmPt.y // 전역 변수에 tm좌표 저장
     }
 
-
-
     private fun stationDust(onStationDustComplete: (String) -> Unit) { //측정소 API 불러오는 코드
-        //var keyword = binding.edtProduct.text.toString()
-        getTm()
+        getTm() // tm좌표 계산 -> 파라미터로 사용
         val call: Call<MYModel> = MyApplication.retroInterface.getRetrofit(
             tmX.toString(),
             tmY.toString(),
@@ -247,11 +234,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                     } else {
                         Log.d("stationDust", "items 리스트가 비어있습니다.")
                     }
-
-                    //Log.d("mobileApp", "${response.body()?.body?.items?:emptyList()}")
-                    //binding.retrofitRecyclerView.layoutManager = LinearLayoutManager(context)
-                    //binding.
-                    //    .adapter = RetrofitAdapter(this, response.body()!!.body.items)
                 }
             }
 
@@ -261,9 +243,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         })
     }
 
-    private fun stationFineDust(stationName: String, callback: (pm10: String, pm25: String) -> Unit) { //미세먼지 API 불러오기
+    // 측정소 이름을 파라미터로 하여 미세먼지 정보 불러오기
+    private fun stationFineDust(stationName: String, callback: (pm10: String, pm25: String) -> Unit) {
         val call: Call<MySModel> = MyApplication.retroInterface2.getRetrofit2(
-            stationName, //측정소이름
+            stationName, // 측정소 이름
             "month",
             "1",
             "100",
@@ -280,8 +263,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                     val pm10value= response.body()?.response?.body?.items?.get(0)?.pm10Value
                     val pm25value= response.body()?.response?.body?.items?.get(0)?.pm25Value
                     callback(pm10value.toString(), pm25value.toString())
-                    //binding.retrofitRecyclerView.layoutManager = LinearLayoutManager(context)
-                    //binding.retrofitRecyclerView.adapter = MyRetrofitAdapter(this, response.body()!!.body.items)
                 }
             }
 
@@ -291,21 +272,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         })
     }
 
-
-    /*
-        private fun allOfStation(stationName: String, sidoName: String): LatLng? = runBlocking {// 측정소 위치값 return 받을 때 응답속도가 느려서 자꾸 null이 반환되는 문제가 생겨서 동기화 함수 추가 작성함
-        var coord: LatLng? = null
-
-        val deferredCoord = async(Dispatchers.IO) {
-            fetchStationCoordinates(stationName, sidoName)
-        }
-
-        coord = deferredCoord.await()
-
-        return@runBlocking coord
-    }*/
-
-    private suspend fun fetchStationCoordinates(stationName: String, sidoName: String): LatLng? { //미세먼지->측정소 API 불러오기
+    // 측정소 이름으로 측정소 위치 받아오기
+    private suspend fun fetchStationCoordinates(stationName: String, sidoName: String): LatLng? {
         val call: Call<MyBModel> = MyApplication.retroInterface4.getRetrofit4(
             "uItfMom3tDSQvZa3Xm2GwUrA5YidOSP4H1qHM/rkupqT9pT5TNa4zyQWdXFnbKlKSqBZsEqJtZrQfYYrPHAwgg==",
             "json",
@@ -314,7 +282,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             sidoName,
             stationName
         )
-
         try { // api에서 반환해주는 dmx, dmy(위도와 경도) 값을 이용해 측정소 위치 return
             val response = call.execute()
             if (response.isSuccessful) {
@@ -329,13 +296,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         } catch (e: Exception) {
             Log.d("mobileApp", e.toString())
         }
-
         return null
     }
 
     private suspend fun fetchStationCoordinatesAsync(stationName: String, sidoName: String): LatLng? {
         return withContext(Dispatchers.IO) {
-
             fetchStationCoordinates(stationName, sidoName)
         }
     }
@@ -381,12 +346,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 _binding.headerName!!.text= ""
             }
         }
-
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            R.id.item1 -> {Log.d("mobileApp", "네비게이션 뷰 메뉴 1")}
+            R.id.item1 -> {Log.d("mobileApp", "네비게이션 뷰 메뉴 1")} // 지워도되나
             R.id.item2 -> {
                 if (item.itemId === R.id.item2) {
                     val intent = Intent(this, AlarmActivity::class.java)
@@ -394,7 +358,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                     startActivity(intent)
                 }
             }
-            R.id.item3 -> {Log.d("mobileApp", "네비게이션 뷰 메뉴 3")}
+            R.id.item3 -> {Log.d("mobileApp", "네비게이션 뷰 메뉴 3")} // 지워도되나
             R.id.item4 -> {
                 if(item.itemId === R.id.item4){
                     val intent = Intent(this, AuthActivity::class.java)
@@ -486,11 +450,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                                     return true
                                 }
                             })
-
                             }
                         }
                     } else {
-                        // Handle the case when the response body is null
+                        // Handle the case when the response is not successful
                     }
                 } else {
                     // Handle the case when the response is not successful
@@ -505,7 +468,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onMapReady(naverMap: NaverMap) { //네이버 지도의 이벤트를 처리하는 콜백함수
-
         // NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
         marker = Marker()
         // 지도상에 마커 표시
@@ -566,27 +528,17 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return
         }
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location: Location? ->
                 if (location != null) {
                     currentLocation = location
-                    // 위치 오버레이의 가시성은 기본적으로 false로 지정되어 있습니다. 가시성을 true로 변경하면 지도에 위치 오버레이가 나타납니다.
                     // 파랑색 점, 현재 위치 표시
                     naverMap.locationOverlay.run {
-                        isVisible = true
+                        isVisible = true // 위치 오버레이의 가시성은 기본적으로 false로 지정되어 있습니다. 가시성을 true로 변경하면 지도에 위치 오버레이가 나타납니다.
                         position = LatLng(currentLocation!!.latitude, currentLocation!!.longitude)
-
                     }
-
                     // 카메라 현재위치로 이동
                     val cameraUpdate = CameraUpdate.scrollTo(
                         LatLng(
@@ -595,33 +547,28 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                         )
                     )
                     naverMap.moveCamera(cameraUpdate)
-
                     // 빨간색 마커 현재위치로 변경
                     marker.position = LatLng(
                         naverMap.cameraPosition.target.latitude,
                         naverMap.cameraPosition.target.longitude
                     )
-
+                    // 가장 최근 위치 저장
                     latelyPosition = LatLng(
                         naverMap.cameraPosition.target.latitude,
                         naverMap.cameraPosition.target.longitude
                     )
                 }
             }
-
         naverMap.addOnLocationChangeListener { location ->
             lat = location.latitude
             lon = location.longitude
-
         }
 
-        //marker누르면 상세정보 페이지 띄우기
+        //marker 누르면 상세정보 페이지 띄우기
         marker.setOnClickListener {
             stationDust { firstItem ->
                 val station = firstItem
                 val marker_address = getAddress(marker.position.latitude, marker.position.longitude)
-
-
                 // stationFineDust 함수 사용
                 stationFineDust(station) { pm10value, pm25value ->
                     // 콜백으로 전달된 pm10value를 이용하여 InfoActivity를 시작
@@ -630,14 +577,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                     intent.putExtra("pm25value", pm25value)
                     intent.putExtra("stationvalue", station)
                     intent.putExtra("addressvalue", marker_address)
-
                     startActivity(intent)
                 }
             }
             true
         }
     }
-
 
     // 좌표 -> 주소 변환     marker.setonclicklistener 에서 사용함
     private fun getAddress(lat: Double, lng: Double): String {
@@ -660,7 +605,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                     addressResult = currentLocationAddress
                 }
             }
-
         } catch (e: IOException) {
             e.printStackTrace()
         }
@@ -682,9 +626,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
+    // 권한 체킹
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
         // request code와 권한획득 여부 확인
         if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
